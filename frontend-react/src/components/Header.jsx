ed377b86bf0d61ea0923a77e1534fc7d02485265
FranciscoAmaro <famaro@visalogistica.com>
2025-04-15 15:12:47 -0600

    Login functionaliti

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
-import Button from './Button'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import Button from "./Button";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProvider";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
   return (
     <>
-    <nav
-    className='navbar container pt-3 pb-3 align-items-start'
-    >
-        <Link className='navbar-brand text-light' to="/">Stock Prediction Portal</Link>
-        <div >
-            <Button text='Login' className='btn-outline-info'
-            url={'/login'} />
-            &nbsp;
-            <Button text='Register' className='btn-info' url={'/register'}/>
+      <nav className="navbar container pt-3 pb-3 align-items-start">
+        <Link className="navbar-brand text-light" to="/">
+          Stock Prediction Portal
+        </Link>
+        <div>
+          {isLoggedIn ? (
+            <button className="btn btn-danger" onClick={handleLogout}>
+              Logout
+            </button>
+          ) : (
+            <>
+              <Button
+                text="Login"
+                className="btn-outline-info"
+                url={"/login"}
+              />
+              &nbsp;
+              <Button text="Register" className="btn-info" url={"/register"} />
+            </>
+          )}
         </div>
-    </nav>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider";
 
 const Login = () => {
-  return (
-    <div className='text-light'>Login</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState('');
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext);
+  const navigate = useNavigate();
 
-export default Login
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const userData = { username, password };
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/token/",
+        userData
+      );
+      if (response.status === 200) {
+        localStorage.setItem("access", response.data.access);
+        localStorage.setItem("refresh", response.data.refresh);
+        setIsLoggedIn(true);
+        navigate("/");
+      }
+    } catch (err) {
+      console.error("Login error", err.response.data);
+      setErrors("Invalid credentials");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 bg-light-dark p-5 rounded">
+            <h3 className="text-light text-center mb-4">
+              Loggin to our Portal
+            </h3>
+            <form onSubmit={handleLogin}>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Set password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              {errors && (
+                <div className="text-danger text-center mb-3">{errors}</div>
+              )}
+
+              {loading ? (
+                <button
+                  type="submit"
+                  className="btn btn-info w-100 mb-3"
+                  disabled
+                >
+                  <FontAwesomeIcon icon={faSpinner} spin />
+                  Logging in...
+                </button>
+              ) : (
+                <button type="submit" className="btn btn-info w-100 mb-3">
+                  Login
+                </button>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
